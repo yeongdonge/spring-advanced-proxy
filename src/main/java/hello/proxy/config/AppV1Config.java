d21cc278cfd2b8cdc44944a4db4a9a361cf9ed63
yeongdonge <parrineau@gmail.com>
2022-06-12 17:17:11 +0900

    인터페이스 <-> 구현 클래스 케이스 구현, 구체 클래스 케이스 구현, 컨포넌트 스캔 케이스 구현 -> 프록시 개념 실습 전 요구사항

---
+++ src/main/java/hello/proxy/config/AppV1Config.java
@@ -0,0 +1,26 @@
+package hello.proxy.config;
+
+import hello.proxy.app.v1.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV1Config {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1() {
+        return new OrderControllerV1Impl(orderServicev1());
+    }
+
+    @Bean
+    public OrderServiceV1 orderServicev1() {
+        return new OrderServiceV1Impl(orderRepositoryV1());
+
+
+    }
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1() {
+        return new OrderRepositoryV1Impl();
+    }
+
+}
