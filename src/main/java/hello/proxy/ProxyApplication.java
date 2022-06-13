563564cff97157dcd635522daaa74db8edfa4606
yeongdonge <parrineau@gmail.com>
2022-06-13 21:31:03 +0900

    구체 클래스 기반 프록시 서비스 적용 및 테스트

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 //@Import({AppV2Config.class, AppV1Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 public class ProxyApplication {
 
 	public static void main(String[] args) {
