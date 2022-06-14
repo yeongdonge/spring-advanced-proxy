341daea6509fa3b4ca2dc975d711e23c2fa401bf
yeongdonge <parrineau@gmail.com>
2022-06-14 18:30:14 +0900

    동적 프록시 Reflection 기본 구현 및 테스트 JDK 동적 프록시 기본 구현 및 테스트 JDK 동적 프록시 실제 적용 및 테스트

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 //@Import({AppV2Config.class, AppV1Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 public class ProxyApplication {
 
 	public static void main(String[] args) {
